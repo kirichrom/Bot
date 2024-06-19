72ac0d862df3a9bfd85b3e74811cb3aca4d650f1
kirill.romanovski <kirill.romanovski@gmail.com>
2024-06-19 18:51:05 +0300

    new

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.11
+
 WORKDIR /app
-COPY requirements.txt requirements.txt
-RUN pip3 install --upgrade setuptools
-RUN pip3 install -r requirements.txt
-RUN chmod 755 .
+
 COPY . .
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "__main__.py"]
--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import sys
+from os import getenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -8,14 +9,7 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-
-import os
-from dotenv import load_dotenv
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
-API_TOKEN = os.getenv('TOKEN')
+API_TOKEN = getenv("API_TOKEN")
 
 dp = Dispatcher()
 
@@ -29,16 +23,20 @@ async def command_start_handler(message: Message) -> None:
 async def echo_handler(message: Message) -> None:
     try:
         await message.send_copy(chat_id=message.chat.id)
+        logging.info(f'Received message: {message.text}')
     except TypeError:
         await message.answer("Nice try!")
 
 
 async def main() -> None:
     bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logging.error(f"Error in main: {e}")
+        sys.exit(1)
